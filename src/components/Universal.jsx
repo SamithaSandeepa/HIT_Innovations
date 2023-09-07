bd477839390dc7fbb84f0aeb43342d4f0f99fe78
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2023-09-07 19:23:39 +0530

    universal energy

--- src/components/Universal.jsx
+++ src/components/Universal.jsx
@@ -1,10 +1,21 @@
 import { apple, bill, google } from "../assets";
 import styles, { layout } from "../style";
+import { Link } from "react-router-dom";
 
 const Universal = () => (
   <section id="product">
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <h2 className={styles.heading2}>Universal Energy</h2>
+    <div className="bg-primary py-5 px-5">
+      <Link
+        to="/"
+        className="px-4 py-2 font-poppins font-medium text-[16px] text-blue-500 bg-primary border border-blue-500 rounded-[10px] outline-none hover:bg-blue-500 hover:text-primary "
+      >
+        &lt;&nbsp;Back
+      </Link>
+    </div>
+    <div className="container mx-auto">
+      <div className="flex flex-col text-center w-full mb-12">
+        <h2 className={styles.headingwhite}>UNIVERSAL ENERGY</h2>
+      </div>
     </div>
   </section>
 );
