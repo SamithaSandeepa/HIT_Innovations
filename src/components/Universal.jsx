a586136c6008021142189add8d6bdae3c3c07e4d
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2023-09-13 17:59:58 +0530

    Update Universal.jsx

--- src/components/Universal.jsx
+++ src/components/Universal.jsx
@@ -8,7 +8,7 @@ const Universal = () => (
       <div className="bg-primary py-5 px-5">
         <Link
           to="/"
-          className="px-4 py-2 font-poppins font-medium text-[16px] text-blue-500 bg-primary border border-blue-500 rounded-[10px] outline-none hover:bg-blue-500 hover:text-primary "
+          className="px-4 py-2 font-poppins font-medium text-[16px] text-blue-500 bg-primary border border-blue-500 rounded-[10px] outline-none hover:bg-blue-500 hover:text-primary"
         >
           &lt;&nbsp;Back
         </Link>
